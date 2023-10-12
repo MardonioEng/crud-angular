39abd3e4150cae21afdb9e90f7acae9397863fd1
Mardonio Rodrigues <mardonio.silva@itegra.com.br>
2023-10-11 22:35:46 -0300

    Form: Spring API: Create Course (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,9 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -15,7 +16,8 @@ export class CourseFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -25,12 +27,19 @@ export class CourseFormComponent {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe({
-      next: (result) => console.log(result),
-      error: (error) => this.onError(),
+      next: () => this.onSuccess(),
+      error: () => this.onError(),
     });
   }
 
-  onCancel() {}
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
+  }
 
   private onError() {
     this._snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
